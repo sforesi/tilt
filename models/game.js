02c54939c8906ac45b7516bd793e1cd184efbf18
kkinge79 <kingerykagan@gmail.com>
2021-12-17 09:34:36 -0600

    game model

---
+++ models/game.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const gameSchema = new mongoose.Schema(
+  {
+    title: String,
+    coverArt: String,
+    genre: String,
+    releaseYear: Number,
+    rating: Number,
+    description: String,
+    reviews: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Review'
+    }
+  }
+)
+
+const Game = mongoose.model('Game', gameSchema)
+
+export {
+  Game
+}
