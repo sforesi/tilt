61dd86046dd8d42e80be7d8c7247bea8948b55c1
kkinge79 <kingerykagan@gmail.com>
2021-12-17 09:04:25 -0600

    styling, addning a new file, mods to signup page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Login from './pages/Login/Login'
 import Landing from './pages/Landing/Landing'
 import Profiles from './pages/Profiles/Profiles'
 import * as authService from './services/authService'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const App = () => {
   const [user, setUser] = useState(authService.getUser())
