ab4ef4a23df474ccee3cdc6d0e77a03def9f73ef
sforesi <sebastiano.foresi@gmail.com>
2021-12-17 13:48:07 -0500

    create posts

--- controllers/games.js
+++ controllers/games.js
@@ -1,6 +1,5 @@
 import { Profile } from '../models/profile.js'
 import { Game } from '../models/game.js'
-import { Collection } from '../models/collection.js'
 
 
 
--- routes/posts.js
+++ routes/posts.js
@@ -10,7 +10,7 @@ const router = Router()
 
 // ========= Protected Routes ========= 
 router.use(decodeUserFromToken)
-
+router.post('/', checkAuth, postCtrl.create)
 
 export {
     router
