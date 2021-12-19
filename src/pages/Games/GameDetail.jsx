d5665589408f18b1611c9b86078e9fcc1fc32c3b
sforesi <sebastiano.foresi@gmail.com>
2021-12-19 13:34:13 -0500

    stub up useEffect

--- src/pages/Games/GameDetail.jsx
+++ src/pages/Games/GameDetail.jsx
@@ -1,14 +1,33 @@
+import React, { useEffect } from 'react'
+
+// Services 
 import * as gameService from '../../services/gameService'
-import React from 'react'
+
+// Components
 
 
 const GameDetail = (props) => {
+  const rawg_id = 8821
+
+  useEffect(() => {
+    const fetchGame = async () => {
+      try {
+        const gameData = await gameService.getGame(rawg_id)
+
+      } catch (error) {
+        throw error
+      }
+    }
+    fetchGame()
+  }, [rawg_id])
+
   return (
     <div>
       
     </div>
 
   )
+
 }
   
 export default GameDetail
