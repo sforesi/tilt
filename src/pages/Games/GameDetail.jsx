376ca644d38f62a0561ba515f1bdff23d4827c94
sforesi <sebastiano.foresi@gmail.com>
2021-12-19 16:49:44 -0500

    test

--- src/pages/Games/GameDetail.jsx
+++ src/pages/Games/GameDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 // Services 
@@ -9,13 +9,14 @@ import DisplayInfo from '../../components/Games/DisplayInfo'
 
 const GameDetail = (props) => {
   const {rawg_id} = useParams()  
+  const [games, setGames] = useState([])
 // 8821
   useEffect(() => {
     const fetchGame = async () => {
       try {
         const gameData = await gameService.getGame(rawg_id)
         console.log('see me', gameData)
-
+        setGames(gameData)
       } catch (error) {
         throw error
       }
@@ -26,7 +27,7 @@ const GameDetail = (props) => {
   return (
     <div>
       <DisplayInfo
-      
+      games={games}
       />
     </div>
 
