413f73bebed5936acefe30377057b7066aa94ad3
sforesi <sebastiano.foresi@gmail.com>
2021-12-19 17:44:27 -0500

    genres and platforms showing

--- src/components/Games/DisplayInfo.jsx
+++ src/components/Games/DisplayInfo.jsx
@@ -7,9 +7,14 @@ const DisplayInfo = (props) => {
       <img src={props.game.background_image} alt="background-img" />
       <h1>Game Details</h1>
       <h3>{props.game.name}</h3>
+      <h3>{props.game.genres[0].name} / {props.game.genres[1].name}</h3>
       <h3>{props.game.released}</h3>
-      <h3>{props.game.developers[0].name}</h3>
-      {props.game.description_raw}
+      <h3>{props.game.developers[0].name}</h3>   
+      <h3>{props.game.platforms[0].platform.name}</h3>
+      
+
+
+      <p>{props.game.description_raw}</p>
 
     </div>
   )
