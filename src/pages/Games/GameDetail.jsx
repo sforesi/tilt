2ebb5046b8c1c0789236b4f4aff7c6be5400db51
sforesi <sebastiano.foresi@gmail.com>
2021-12-19 13:03:57 -0500

    add gameService function. getAllGames and getGame.

---
+++ src/pages/Games/GameDetail.jsx
@@ -0,0 +1,14 @@
+import * as gameService from '../../services/gameService'
+import React from 'react'
+
+
+const GameDetail = (props) => {
+  return (
+    <div>
+      
+    </div>
+
+  )
+}
+  
+export default GameDetail
