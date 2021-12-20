51126108898142bcbd91995e8907d081e95b0b42
sforesi <sebastiano.foresi@gmail.com>
2021-12-20 18:31:36 -0500

    divs established

--- src/components/Games/DisplayInfo.jsx
+++ src/components/Games/DisplayInfo.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
-import './DisplayInfo.css'
+import React from "react";
+import "./DisplayInfo.css";
 const DisplayInfo = (props) => {
-  console.log(props.game.rawg.name)
+  console.log(props.game.rawg.name);
   return (
-    <div className='game-detail'>
-      <div className='bg-image'>
-
-        <img id='bg-image' src={props.game.rawg.background_image} alt="background-img" />
+    <div className="game-detail">
+      <div className="left-column">
+        <img
+          id="bg-image"
+          src={props.game.rawg.background_image}
+          alt="background-img"
+        />
       </div>
-      <h3>{props.game.rawg.name}</h3>
-      <h3>{props.game.rawg.genres.map(g => `${g.name} | `)}</h3>
-      <h3>{props.game.rawg.released}</h3>
-      <h3>{props.game.rawg.developers[0].name}</h3>   
-      <h3>{props.game.rawg.platforms.map(p => `${p.platform.name} | `)}</h3>
-      {/* <ul>
-        {props.game.short_screenshots.map(ss => <li><img src={ss.image} alt="screenshot" /></li>)}
-      </ul> */}
-      
-
-
-      <p>{props.game.rawg.description_raw}</p>
-
+      <div className="right-column">
+        <p>{props.game.rawg.name}</p>
+        <p>{props.game.rawg.genres.map((g) => `${g.name} | `)}</p>
+        <p>{props.game.rawg.released}</p>
+        <p>{props.game.rawg.developers[0].name}</p>
+        <p>{props.game.rawg.platforms.map((p) => `${p.platform.name} | `)}</p>
+        <p>{props.game.rawg.description_raw}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DisplayInfo
+export default DisplayInfo;
