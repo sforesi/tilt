0f22f97fbdb19f7fdebfed28e29cfbfbd2b54de1
sforesi <sebastiano.foresi@gmail.com>
2021-12-19 20:37:26 -0500

    remove position absolute

--- src/components/Games/DisplayInfo.jsx
+++ src/components/Games/DisplayInfo.jsx
@@ -8,12 +8,14 @@ const DisplayInfo = (props) => {
 
         <img id='bg-image' src={props.game.background_image} alt="background-img" />
       </div>
-      <h1>Game Details</h1>
       <h3>{props.game.name}</h3>
       <h3>{props.game.genres.map(g => `${g.name} | `)}</h3>
       <h3>{props.game.released}</h3>
       <h3>{props.game.developers[0].name}</h3>   
       <h3>{props.game.platforms.map(p => `${p.platform.name} | `)}</h3>
+      {/* <ul>
+        {props.game.short_screenshots.map(ss => <li><img src={ss.image} alt="screenshot" /></li>)}
+      </ul> */}
       
 
 
