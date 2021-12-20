86779b9eafc6415a3e2debe7da89cb3b7c3b179d
sforesi <sebastiano.foresi@gmail.com>
2021-12-19 20:26:33 -0500

    template literals to print genres and platforms

--- src/components/Games/DisplayInfo.jsx
+++ src/components/Games/DisplayInfo.jsx
@@ -4,13 +4,16 @@ const DisplayInfo = (props) => {
   console.log(props.game)
   return (
     <div className='game-detail'>
-      <img src={props.game.background_image} alt="background-img" />
+      <div className='bg-image'>
+
+        <img id='bg-image' src={props.game.background_image} alt="background-img" />
+      </div>
       <h1>Game Details</h1>
       <h3>{props.game.name}</h3>
-      <h3>{props.game.genres[0].name} / {props.game.genres[1].name}</h3>
+      <h3>{props.game.genres.map(g => `${g.name} | `)}</h3>
       <h3>{props.game.released}</h3>
       <h3>{props.game.developers[0].name}</h3>   
-      <h3>{props.game.platforms[0].platform.name}</h3>
+      <h3>{props.game.platforms.map(p => `${p.platform.name} | `)}</h3>
       
 
 
