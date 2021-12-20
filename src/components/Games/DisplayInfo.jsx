d818a3c7f2280c42b7697550661e44b6a70c9054
sforesi <sebastiano.foresi@gmail.com>
2021-12-20 10:48:51 -0500

    refactor display info

--- src/components/Games/DisplayInfo.jsx
+++ src/components/Games/DisplayInfo.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import './DisplayInfo.css'
 const DisplayInfo = (props) => {
-  console.log(props.game)
+  console.log(props.game.rawg.name)
   return (
     <div className='game-detail'>
       <div className='bg-image'>
 
-        <img id='bg-image' src={props.game.background_image} alt="background-img" />
+        <img id='bg-image' src={props.game.rawg.background_image} alt="background-img" />
       </div>
-      <h3>{props.game.name}</h3>
-      <h3>{props.game.genres.map(g => `${g.name} | `)}</h3>
-      <h3>{props.game.released}</h3>
-      <h3>{props.game.developers[0].name}</h3>   
-      <h3>{props.game.platforms.map(p => `${p.platform.name} | `)}</h3>
+      <h3>{props.game.rawg.name}</h3>
+      <h3>{props.game.rawg.genres.map(g => `${g.name} | `)}</h3>
+      <h3>{props.game.rawg.released}</h3>
+      <h3>{props.game.rawg.developers[0].name}</h3>   
+      <h3>{props.game.rawg.platforms.map(p => `${p.platform.name} | `)}</h3>
       {/* <ul>
         {props.game.short_screenshots.map(ss => <li><img src={ss.image} alt="screenshot" /></li>)}
       </ul> */}
       
 
 
-      <p>{props.game.description_raw}</p>
+      <p>{props.game.rawg.description_raw}</p>
 
     </div>
   )
