5895804ac95d6c7a3ab20fb2630ab419c3fc900d
sforesi <sebastiano.foresi@gmail.com>
2021-12-20 21:31:17 -0500

    start styling banner image

--- src/components/Games/DisplayInfo.jsx
+++ src/components/Games/DisplayInfo.jsx
@@ -3,25 +3,35 @@ import "./DisplayInfo.css";
 const DisplayInfo = (props) => {
   console.log(props.game.rawg.name);
   return (
-    <div className="game-detail">
-      <div className="left-column">
-        <div className="image-container">
-          <img
-            id="bg-image"
-            src={props.game.rawg.background_image}
-            alt="background-img"
-          />
+    <div className="container">
+      <div className="banner-image">
+        <img 
+        id="banner-image"
+        src={props.game.rawg.background_image}
+        alt="banner-img"
+        />
+      </div>
+      <div className="game-detail">
+        <div className="left-column">
+          <div className="image-container">
+            <img
+              id="bg-image"
+              src={props.game.rawg.background_image}
+              alt="background-img"
+            />
 
+          </div>
+        </div>
+        <div className="right-column">
+          <p className="title">{props.game.rawg.name}</p>
+          <p>{props.game.rawg.genres.map((g) => `${g.name} | `)}</p>
+          <p>{props.game.rawg.released}</p>
+          <p>{props.game.rawg.developers[0].name}</p>
+          <p>{props.game.rawg.platforms.map((p) => `${p.platform.name} | `)}</p>
+          <p className="description">{props.game.rawg.description_raw}</p>
         </div>
       </div>
-      <div className="right-column">
-        <p className="title">{props.game.rawg.name}</p>
-        <p>{props.game.rawg.genres.map((g) => `${g.name} | `)}</p>
-        <p>{props.game.rawg.released}</p>
-        <p>{props.game.rawg.developers[0].name}</p>
-        <p>{props.game.rawg.platforms.map((p) => `${p.platform.name} | `)}</p>
-        <p className="description">{props.game.rawg.description_raw}</p>
-      </div>
+
     </div>
   );
 };
