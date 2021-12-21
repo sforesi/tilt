0e6d65ea00a65002a4ff2eda042f06385bb66d49
kkinge79 <kingerykagan@gmail.com>
2021-12-21 10:39:59 -0600

    altered image size

--- src/components/Games/SearchCard.jsx
+++ src/components/Games/SearchCard.jsx
@@ -4,7 +4,7 @@ const SearchCard=(props) =>{
   return(
 <div>
   <div className="wrapper">
-  <img  style={{ Width: '1500px', height: "175px",}} src={props.game.background_image} alt='game' />
+  <img className="game-image"  style={{ Width: '1500px', height: "175px",}} src={props.game.background_image} alt='game' />
   </div>
 </div>
     
