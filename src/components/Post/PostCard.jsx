6c51aad15f900fdfc673551d1b27312d3e144329
bnagle22 <naglebenjamin@gmail.com>
2021-12-21 10:23:12 -0500

    map posts

---
+++ src/components/Post/PostCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostCard = (props) => {
+  return (
+    <div className="post-card">
+
+    </div>
+  )
+}
+
+export default PostCard
--- src/pages/PostList.jsx
+++ src/pages/PostList.jsx
@@ -1,20 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import { getAllPosts } from '../services/postService'
 
+// Components
+import PostCard from '../components/Post/PostCard'
+
 const PostList = () => {
   const [posts, setPosts] = useState([])
 
   useEffect(() => {
     const fetchAllPosts = async () => {
       const postData = await getAllPosts()
-      console.log(postData)
+      setPosts(postData)
     }
     fetchAllPosts()
+    return () => { setPosts([]) }
   }, [])
 
   return (
     <div className="layout">
       <h1>Posts</h1>
+      {posts?.map((post) => (
+        <PostCard
+          post={post}
+          key={post._id}
+          />
+      ))}
     </div>
   )
 }
