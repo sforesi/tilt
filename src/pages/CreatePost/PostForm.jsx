57c6d33930032b47c29b1026781a32b67ef1079b
bnagle22 <naglebenjamin@gmail.com>
2021-12-21 13:56:06 -0500

    PostForm

--- src/pages/CreatePost/PostForm.jsx
+++ src/pages/CreatePost/PostForm.jsx
@@ -4,35 +4,37 @@ const PostForm = (props) => {
   return (
     <form className="create-form" onSubmit={props.handleCreatePost}>
       <div className="review-prompt">
-        <label>Enter your review</label>
+        <label Enter review></label>
       </div>
-      <input
-        required
+      <input 
+        required 
         name="review"
         autoComplete='off'
-        placeholder="review"
+        placeholder='review'
         value={props.name}
         onChange={(e) => props.setReview(e.target.value)}
-      />
-      <textarea
+        />
+        <textarea
         className='input-field'
         value={props.name}
       ></textarea>
-
-      <div className="border"></div>
-
-      <div className="review-prompt">
-        <label>Review</label>
-        <button
-          type="button"
-          id="plus-button"
-          onClick={() => props.setToggleCode(!props.toggleReview)}
-        >+</button>
-      </div>
-
-      <div className="border"></div>
-      <button type="submit">Submit</button>
     </form>
+    
+
+    //   <div className="border"></div>
+
+    //   <div className="review-prompt">
+    //     <label>Review</label>
+    //     <button
+    //       type="button"
+    //       id="plus-button"
+    //       onClick={() => props.setToggleCode(!props.toggleReview)}
+    //     >+</button>
+    //   </div>
+
+    //   <div className="border"></div>
+    //   <button type="submit">Submit</button>
+    // </form>
   )
 }
 
