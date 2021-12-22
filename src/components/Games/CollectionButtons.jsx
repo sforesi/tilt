211add4157c2fc48a6fa52b940b8e47c4a75078f
sforesi <sebastiano.foresi@gmail.com>
2021-12-22 18:38:06 -0500

    fix banner and add icons

--- src/components/Games/CollectionButtons.jsx
+++ src/components/Games/CollectionButtons.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import * as gameService from '../../services/gameService'
-// import { RiHeartAddLine } from 'react-icons/fa'
+import playButton from '../../assets/played-icon.png'
+import favButton from '../../assets/favorites-icon.png'
+import wishButton from '../../assets/wishlist-icon.png'
+import '../../styles/DisplayInfo.css'
+
+
 
 const CollectionButtons = ({game}) => {
   console.log(game)
   const gameId = game.game._id
   const handleCollection = async(e) => {
-    console.log('CATEGORY!!!!', e.target.id)
-    console.log('GAME ID!!!!',gameId)
     const data = await gameService.addToCollection(e.target.id, gameId)
     console.log(data)
     
@@ -18,16 +21,19 @@ const CollectionButtons = ({game}) => {
       <button 
       onClick={(e) => handleCollection(e)}
       id="favorites"
-      >FAVORITE
+      >
+        <img src={favButton} alt="" />
       </button>
       <button 
       onClick={(e) => handleCollection(e)}
-      id="played">PLAYED
-  
+      id="played">
+        <img src={playButton} alt="" />
       </button>
       <button 
       onClick={(e) => handleCollection(e)}
-      id="wishlist">WISHLIST
+      id="wishlist">
+        <img src={wishButton} alt="" />
+
       </button>
     </div>
   )
