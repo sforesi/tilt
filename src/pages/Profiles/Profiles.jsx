e0a170a7da0122002674efda77c415b22f924eba
kkinge79 <kingerykagan@gmail.com>
2021-12-22 09:57:03 -0600

    stubbd up collec lost component

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -1,8 +1,6 @@
-import { use } from 'express/lib/router'
 import { useState, useEffect, useRef } from 'react'
 import * as profileService from '../../services/profileService'
-import CollectionButtons from '../../components/Games/CollectionButtons'
-import { propTypes } from 'react-bootstrap/esm/Image'
+
 
 const Profiles = (props) => {
   const [profiles, setProfiles] = useState([])
